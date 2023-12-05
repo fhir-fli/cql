93fbbb6debb4995fdac5e6ca996af5588ce152c1
Dokotela <grey.faulkenberry@mayjuun.com>
2023-12-04 19:46:30 -0500

    minimal update

--- lib/types/cql_code_service_interfaces.dart
+++ lib/types/cql_code_service_interfaces.dart
@@ -30,34 +30,3 @@ abstract class TerminologyProvider {
   Future<List<CqlValueSet>> findValueSetsByOid(String oid);
   Future<CqlValueSet?> findValueSet(String oid, [String? version]);
 }
-
-class ValueSetDictionaryExtended {
-  final Map<String, Map<String, List<ValueSetCode>>> _valueSets;
-
-  ValueSetDictionaryExtended(this._valueSets);
-
-  Map<String, Map<String, List<ValueSetCode>>> get valueSets => _valueSets;
-
-  void addEntry(String oid, String version, List<ValueSetCode> codes) {
-    _valueSets[oid] ??= {};
-    _valueSets[oid]![version] = codes;
-  }
-}
-
-class ValueSetObjectExtended {
-  final Map<String, Map<String, ValueSet>> _valueSetObjects;
-
-  ValueSetObjectExtended(this._valueSetObjects);
-
-  Map<String, Map<String, ValueSet>> get valueSetObjects => _valueSetObjects;
-
-  void addEntry(String oid, String version, ValueSet valueSet) {
-    _valueSetObjects[oid] ??= {};
-    _valueSetObjects[oid]![version] = valueSet;
-  }
-}
-
-abstract class TerminologyProviderExtended {
-  List<ValueSet> findValueSetsByOid(String oid);
-  ValueSet? findValueSet(String oid, [String? version]);
-}
