224ce3bc568fff36013729436156c9ead7185094
Dokotela <grey.faulkenberry@mayjuun.com>
2023-12-12 07:24:45 -0500

    added flutter to test faster

---
+++ lib/cql/android/app/src/main/kotlin/dev/fhirfli/cql/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.fhirfli.cql
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/cql/cql.dart
+++
@@ -1,5 +0,0 @@
-export 'cql-to-elm/cql_to_elm.dart';
-export 'elm/elm.dart';
-export 'engine/engine.dart';
-export 'exceptions/exceptions.dart';
-export 'model/model.dart';
