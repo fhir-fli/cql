e9be10eaa47087bd142ace6389c523ef43ba9ba7
Dokotela <grey.faulkenberry@mayjuun.com>
2024-02-20 16:43:29 -0500

    redo android

--- android/app/src/main/kotlin/com/example/cql/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cql
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/dev/fhirfli/cql/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.fhirfli.cql
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
